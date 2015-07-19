d5d76a345fe7c9b71a6c57c28aed344de3e70676
Ellis Clayton <ellis@ellisclayton.info>
2015-07-19 20:57:00 +1000

    Added sensors, modularised code

--- AirDuino.ino
+++ AirDuino.ino
@@ -6,6 +6,7 @@
 #include <Adafruit_L3GD20_U.h>
 #include <Adafruit_LSM303_U.h>
 #include <Adafruit_Sensor.h>
+#include <math.h>
 #include <Wire.h>
 
 /* Assign a unique ID to the sensors */
@@ -17,19 +18,18 @@ Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
 // For the Adafruit shield, these are the default.
 #define TFT_DC 9
 #define TFT_CS 10
-
 #define trigPin 30
 #define echoPin 32
 
-float seaLevelPressure = 1025.5;
+// TODO: Allow for SLP entry
+float seaLevelPressure = 1028.4;
 
-float lastAltitude = 0;
-long lastDist      = 0;
+int lastAltitude = 0;
+float lastTemp   = 0;
+long lastDist    = 0;
+int lastHeading  = 0;
 
-// Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-// If using the breakout, change pins as desired
-//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
 
 void initSensors()
 {
@@ -56,37 +56,60 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   initSensors();
-  firstText();
+  clearDisplay();
 }
 
 
 void loop(void) {
   sensors_event_t accel_event;
-  sensors_event_t mag_event;
+
+  printAltitude(getAltitude());
+  printTemp(getTemperature());
+  printRange(getRange());
+  printHeading(getHeading());
+
+  delay(250);
+}
+
+
+/*
+ * Inputs
+ */
+
+// Get altitude in feet
+int getAltitude() {
   sensors_event_t bmp_event;
-  sensors_vec_t   orientation;
-
-  /* Calculate the altitude using the barometric pressure sensor */
   bmp.getEvent(&bmp_event);
-  if (bmp_event.pressure)
-  {
-    /* Get ambient temperature in C */
-    float temperature;
-    bmp.getTemperature(&temperature);
-    /* Convert atmospheric pressure, SLP and temp to altitude    */
-    float altitude = bmp.pressureToAltitude(seaLevelPressure,
-                                            bmp_event.pressure,
-                                            temperature) * 3.2808399;
+  if (bmp_event.pressure) {
+    // Convert atmospheric pressure, SLP and temp to altitude, then convert to feet
+    int altitude = round(
+      bmp.pressureToAltitude(
+        seaLevelPressure,
+        bmp_event.pressure,
+        getTemperature()) * 3.2808399);
 
-    printAltitude(altitude);
-    // Serial.print(F(" m; "));
-    // /* Display the temperature */
-    // Serial.print(F("Temp: "));
-    // Serial.print(temperature);
-    // Serial.print(F(" C"));
+    return altitude;
+  } else {
+    return lastAltitude;
   }
+}
 
-  long duration, distance;
+// Get heading
+int getHeading() {
+  sensors_event_t mag_event;
+  sensors_vec_t orientation;
+  mag.getEvent(&mag_event);
+  if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation)) {
+    return (int) orientation.heading;
+  } else {
+    return lastHeading;
+  }
+}
+
+// Get ranger distance
+int getRange() {
+  long duration;
+  long distance;
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -95,43 +118,75 @@ void loop(void) {
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
   if (distance >= 200 || distance <= 0){
-    Serial.println("Out of range");
+    return (int) -1;
   }
   else {
-    printRange(distance);
+    return (int) distance;
   }
-  delay(250);
 }
 
-unsigned long firstText() {
+// Get temperature
+float getTemperature() {
+  float temperature;
+  bmp.getTemperature(&temperature);
+
+  return temperature;
+}
+
+
+/*
+ * Outputs
+ */
+
+// Fill the screen black
+void clearDisplay() {
   tft.fillScreen(ILI9341_BLACK);
-  unsigned long start = micros();
-  tft.setCursor(120, 0);
-  tft.setTextColor(ILI9341_RED);    tft.setTextSize(3);
-  tft.println("Ranger");
-  return micros() - start;
 }
 
-void printRange(long dist) {
+void printRange(int dist) {
   if (lastDist != dist) {
-    tft.setCursor(0, 60);
+    tft.setCursor(0, 0);
     tft.setTextColor(ILI9341_BLACK);    tft.setTextSize(6);
     tft.println(lastDist);
-    tft.setCursor(0, 60);
+    tft.setCursor(0, 0);
     tft.setTextColor(ILI9341_RED);    tft.setTextSize(6);
     tft.println(dist);
     lastDist = dist;
   }
 }
 
-void printAltitude(float altitude) {
+void printAltitude(int altitude) {
   if (lastAltitude != altitude) {
-    tft.setCursor(0, 120);
+    tft.setCursor(0, 60);
     tft.setTextColor(ILI9341_BLACK);    tft.setTextSize(6);
     tft.println(lastAltitude);
-    tft.setCursor(0, 120);
+    tft.setCursor(0, 60);
     tft.setTextColor(ILI9341_RED);    tft.setTextSize(6);
     tft.println(altitude);
     lastAltitude = altitude;
+  }
+}
+
+void printHeading(int heading) {
+  if (lastHeading != heading) {
+    tft.setCursor(0, 180);
+    tft.setTextColor(ILI9341_BLACK);    tft.setTextSize(6);
+    tft.println(lastHeading);
+    tft.setCursor(0, 180);
+    tft.setTextColor(ILI9341_RED);    tft.setTextSize(6);
+    tft.println(heading);
+    lastHeading = heading;
+  }
+}
+
+void printTemp(float temp) {
+  if (lastTemp != temp) {
+    tft.setCursor(0, 120);
+    tft.setTextColor(ILI9341_BLACK);    tft.setTextSize(6);
+    tft.println(lastTemp);
+    tft.setCursor(0, 120);
+    tft.setTextColor(ILI9341_RED);    tft.setTextSize(6);
+    tft.println((int) temp);
+    lastTemp = temp;
   }
 }
