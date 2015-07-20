f5f02452b03f2a2f9f15c4b1e1bca97ec97642a3
Ellis Clayton <ellis@ellisclayton.info>
2015-07-20 20:33:54 +1000

    Added SD logging

--- AirDuino.ino
+++ AirDuino.ino
@@ -7,6 +7,7 @@
 #include <Adafruit_LSM303_U.h>
 #include <Adafruit_Sensor.h>
 #include <math.h>
+#include <SD.h>
 #include <Wire.h>
 
 /* Assign a unique ID to the sensors */
@@ -15,9 +16,10 @@ Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
 Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
 
-// For the Adafruit shield, these are the default.
+// Pin declarations
 #define TFT_DC 9
 #define TFT_CS 10
+#define SD_CS 4
 #define trigPin 30
 #define echoPin 32
 
@@ -29,10 +31,53 @@ float lastTemp   = 0;
 long lastDist    = 0;
 int lastHeading  = 0;
 
+long totalTime = 0;
+
+File logFile;
+
+String logNameString = String();
+char logName[11];
+
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
-void initSensors()
-{
+void createLogName(int fileCount) {
+  logNameString = "log";
+  logNameString += threeDigit((String) fileCount) + ".csv";
+  logNameString.toCharArray(logName, sizeof(logName));
+}
+
+void initSD() {
+  pinMode(TFT_CS, OUTPUT);
+  if (!SD.begin(SD_CS)) {
+    errorMsg("SD initialisation failed");
+  }
+
+  // Create initial file name
+  int fileCount = 0;
+  createLogName(fileCount);
+
+  bool fileExists = true;
+
+  // Increase file name index until new file is found
+  while(fileExists) {
+    if (!SD.exists(logName)) {
+      logFile = SD.open(logName, FILE_WRITE);
+      logFile.println("Altitude,Temperature,Range,Heading");
+      logFile.close();
+
+      fileExists = false;
+    } else {
+      fileCount++;
+      createLogName(fileCount);
+    }
+  }
+
+  if (!SD.exists(logName)) {
+    errorMsg("Log file not created");
+  }
+}
+
+void initSensors() {
   if(!accel.begin()) {
     /* There was a problem detecting the LSM303 ... check your connections */
     Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
@@ -53,6 +98,7 @@ void initSensors()
 void setup() {
   tft.begin();
   tft.setRotation(1);
+  initSD();
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   initSensors();
@@ -62,11 +108,25 @@ void setup() {
 
 void loop(void) {
   sensors_event_t accel_event;
+  int altitude      = getAltitude();
+  float temperature = getTemperature();
+  int range         = getRange();
+  int heading       = getHeading();
 
-  printAltitude(getAltitude());
-  printTemp(getTemperature());
-  printRange(getRange());
-  printHeading(getHeading());
+  String logMsg = (String) altitude;
+  logMsg += ",";
+  logMsg += (int) temperature;
+  logMsg += ",";
+  logMsg += (String) range;
+  logMsg += ",";
+  logMsg += (String) heading;
+
+  logToCard(logMsg);
+
+  printAltitude(altitude);
+  printTemp(temperature);
+  printRange(range);
+  printHeading(heading);
 
   delay(250);
 }
@@ -189,4 +249,39 @@ void printTemp(float temp) {
     tft.println((int) temp);
     lastTemp = temp;
   }
+}
+
+// Report error message
+void errorMsg(String msg) {
+  clearDisplay();
+  tft.setCursor(0, 0);
+  tft.setTextColor(ILI9341_RED);    tft.setTextSize(2);
+  tft.println(msg);
+
+  while(1);
+}
+
+// Log to SD
+void logToCard(String msg) {
+  if (millis() >= totalTime + 1000) {
+    logFile = SD.open(logName, FILE_WRITE);
+
+    if (logFile) {
+      logFile.println(msg);
+      logFile.close();
+    }
+    totalTime = millis();
+  }
+}
+
+// Pad Zeros for a three digit String
+String threeDigit(String input) {
+  if (input.length() < 3) {
+    int length = input.length();
+    for (int i = 0; i < 3 - length; i++) {
+      input = "0" + input;
+    }
+  }
+
+  return input;
 }
